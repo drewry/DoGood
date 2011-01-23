500aaae80121461a92b90515f09f0ccb93d2b57a
Drew Morris <dhmorris@gmail.com>
2011-01-23 16:46:45 -0600

    Fixing some bugs here and there

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     # if the user is signed in send them to the main goods page
     if user_signed_in?
-      redirect_to :controller => "goods"
+      redirect_to new_good_path
     end
   end
 
